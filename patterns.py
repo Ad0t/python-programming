057cb24931234bb3543989b599c7b2ad7ec1c72f
Ad0t <171239099+Ad0t@users.noreply.github.com>
2025-06-17 23:24:35 +0530

    17-06-2025

--- patterns.py
+++ patterns.py
@@ -1,6 +1,41 @@
 # n = int(input())
 n = 4
+
+def sq_pat(n):
+    for i in range(n):
+        for j in range(n):
+            print("*", end="\t")
+        print("\n")
+
+def tri_pat1(n):
+    for i in range(n):
+        for j in range(i+1):
+            print("*", end="\t")
+        print("\n")
+
+def tri_pat2(n):
+    for i in range(n):
+        for j in range(n-i):
+            print("*", end="\t")
+        print("\n")
+
+
 for i in range(n):
-    for j in range(n):
+    for j in range(n-i-1):
+        print(" ", end="\t")
+    for j in range(i+1):
         print("*", end="\t")
-    print()
+    print("\n")
+print("-"*30)
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ", end="\t")
+    for j in range(i+1):
+        print("*", end="\t")
+    # print("\n")
+    for j in range(i):
+        print("*", end="\t")
+    print("\n")
+print("-"*30)
+
